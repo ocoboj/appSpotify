21e4f11a6847af98ebd860dc1bbd7bd4ae9de93e
ocoboj <olgaconesaboj@gmail.com>
2021-10-11 16:43:19 +0200

    Router Oulet

---
+++ spotify/src/app/modules/favorites/favorites.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FavoritePageComponent } from './pages/favorite-page/favorite-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    FavoritePageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FavoritesModule { }
