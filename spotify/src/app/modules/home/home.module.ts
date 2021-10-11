efcb928a3121479203598df63a1368cfbab88308
ocoboj <olgaconesaboj@gmail.com>
2021-10-11 13:30:13 +0200

    Componentes de página

--- spotify/src/app/modules/home/home.module.ts
+++ spotify/src/app/modules/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
